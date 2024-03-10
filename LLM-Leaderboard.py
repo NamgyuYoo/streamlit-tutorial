b92968bcec8566a343335f7c0da0cd5824e85b9f
NamgyuYoo <ryu7424@naver.com>
2024-03-10 09:14:58 +0900

    Update LLM-Leaderboard.py

--- LLM-Leaderboard.py
+++ LLM-Leaderboard.py
@@ -10,33 +10,53 @@ st.title('CP S/W LLM Benchmark')
 st.title('Leaderoard :sports_medal:')
 
 filepath =  'D:\LLMWorkSpace\streamlit-tutorial\leaderboard.csv'
-df =  pd.read_csv(filepath, encoding='cp949')
+df = pd.read_csv(filepath, encoding='cp949')
+
 
 benchmarklist = df.columns[3:]
 modellist = df['Model Name']
+resultslist = df.columns[2:]
 
-print('benchmarklist:',benchmarklist)
-print('modellist:',modellist)
+
+df = df.set_index(keys=modellist, inplace=False, drop=False)
+
+#print('benchmarklist:',benchmarklist)
+#print('modellist:',modellist)
 
 addfilters = st.checkbox('Add filters')
 
 if addfilters:
+      #rows
+    dispdf = df.copy()
+    
+    #dispdf = dispdf.set_index(keys=modellist, inplace=False, drop=False)
+    
+   
     filterbymodel = st.multiselect(
-    'Filter by model:', ['Green', 'Yellow', 'Red', 'Blue'], default=None)
-
+    'Filter by model:', modellist, default=None)
+    if filterbymodel:
+        dispdf =  dispdf.loc[filterbymodel,:]
     st.write('You selected:', filterbymodel)
     
+    #columns
     filterbybenchmark = st.multiselect(
-    'Filter by benchmark:', ['Green', 'Yellow', 'Red', 'Blue'], default=None)
+    'Filter by benchmark:', benchmarklist, default=None)
+    if filterbybenchmark:
+        dispdf =  dispdf.loc[:,filterbybenchmark]
 
     st.write('You selected:', filterbybenchmark)
     
     filterbyresults = st.multiselect(
-    'Filter by results:', ['Green', 'Yellow', 'Red', 'Blue'], default=None)
+    'Filter by results:', resultslist, default=None)
 
     st.write('You selected:', filterbyresults)
     
-   
+    print('filterbymodel:',filterbymodel)
+    print('filterbybenchmark:',filterbybenchmark)
+    print('filterbyresults:',filterbyresults)
+    
+     
+    st.dataframe(dispdf) # Row
     
 clearemptyresults = st.checkbox('Clear empty entries')
 
@@ -44,6 +64,6 @@ if clearemptyresults:
     st.write('clearemptyresults selected!')
 
 
+st.dataframe(df)  
 
-st.dataframe(df)  # Same as st.write(df)
 
